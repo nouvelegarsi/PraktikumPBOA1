d0aa916f85557541a4704310279d6879bd3a6c8b
Nouvella Rahma Fitrah Legarsi <emuf.legarsi@gmail.com>
2026-04-05 18:54:16 +0700

    Perbaikan pertemuan6

---
+++ Pertemuan6_1/src/AngkaSialException.java
@@ -0,0 +1,12 @@
+/* Nama File    : AngkaSialException.java
+ * Deskripsi    : Eksepsi buatan sendiri, menolak masukan angka 13!
+ * Pembuat      : Nouvella Rahma Fitrah Legarsi
+ * Tanggal      : 26 Maret 2026
+ */
+
+public class AngkaSialException extends Exception {
+    public AngkaSialException(){
+        super("jangan memasukkan angka 13 karena angka sial !!!");
+    }
+} 
+    
